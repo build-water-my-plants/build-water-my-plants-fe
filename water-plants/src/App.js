0196b39c0366fae16ac199be6b45813c85732b34
kly001 <karenly1961@gmail.com>
2019-10-16 12:27:51 -0400

    Setting up folders

--- water-plants/src/App.js
+++ water-plants/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+// import { BrowserRouter as Router, Route,Switch, Link } from 'react-router-dom';
+import Login from "./components/Login"
+import Signup from "./components/Signup"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1> Build Water My Plants - FrontEnd</h1>
-     <h3> Objectives:</h3>
+     <img src="https://images.pexels.com/photos/1906439/pexels-photo-1906439.jpeg?cs=srgb&dl=house-plant-plants-pot-plants-1906439.jpg&fm=jpg" alt="succulents"/>
+     {/* <h3> Objectives:</h3>
      <h4>Login Page & Signup Page including-</h4>
      <p> JSON Web Tokens</p>
      <p>React</p>
      <p>Protected Routes</p>
-    <p>Semantic UI and/or Styled Components</p>
+    <p>Semantic UI and/or Styled Components</p> */}
+
+    <h2>Forms</h2>
+    <Login />
+    {/* <Signup /> */}
     </div>
   );
 }
---
+++ water-plants/src/components/Login.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+class Login extends React.Component {
+  state = {
+    creds: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = ev => {
+    this.setState({
+      creds: {
+        ...this.state.creds,
+        [ev.target.name]: ev.target.value
+      }
+    });
+  };
+
+  login = ev => {
+    ev.preventDefault();
+    axiosWithAuth()
+      .post("/login", this.state.creds)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/member");
+      })
+      .catch(error => console.log(error));
+  };
+
+  render() {
+    return (
+      <div className="login">
+       <h4>Login Page</h4>
+        <img src="https://images.pexels.com/photos/1845290/pexels-photo-1845290.jpeg?cs=srgb&dl=houseplant-plants-pot-plants-1845290.jpg&fm=jpg" alt="succulents" />
+      
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
