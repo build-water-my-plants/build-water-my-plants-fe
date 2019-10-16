f7da8cc155f7aee59d10d32e76e7e67a01eb1570
kly001 <karenly1961@gmail.com>
2019-10-16 11:46:17 -0400

    Create-React-App initialized

---
+++ water-plants/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Build Water My Plants - FrontEnd</h1>
+     <h3> Objectives:</h3>
+     <h4>Login Page & Signup Page including-</h4>
+     <p> JSON Web Tokens</p>
+     <p>React</p>
+     <p>Protected Routes</p>
+    <p>Semantic UI and/or Styled Components</p>
+    </div>
+  );
+}
+
+export default App;
