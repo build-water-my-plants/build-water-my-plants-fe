17492868b0e9a1485c80083d3b39ebd6224b1e0c
kly001 <karenly1961@gmail.com>
2019-10-23 00:33:09 -0400

    Reverted back to working code; needs to be merged

---
+++ water-plants/src/reducers/index.js
@@ -0,0 +1,67 @@
+import {GET_USERS, GET_SUCCESS, GET_FAILURE} from "../actions"
+
+
+
+const initialState = {
+
+  users:[],
+
+  getUsers: false,
+
+  addUser: false,
+
+}
+
+
+
+export const userReducer = (state = initialState,action) => {
+
+  switch (action.type) {
+
+    case GET_USERS:
+
+    return {
+
+      ...state,
+
+      error: "",
+
+      getUsers: true,
+
+    };
+
+  case GET_SUCCESS:
+
+    return { 
+
+      ...state,
+
+      getUsers: false,
+
+      usersfs: action.payload,
+
+      error: "",
+
+    };
+
+  case GET_FAILURE:
+
+    return {
+
+     ...state,
+
+     getUsers: false,
+
+     error: action.payload,    
+
+    };
+
+    
+
+  default:
+
+    return state;
+
+  }
+
+}
